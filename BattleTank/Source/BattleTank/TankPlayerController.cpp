6609daa08a197e95c657a3cac2fc240028ca0f55
STEVIEPC\Stephen <porter.stephen68@gmail.com>
2018-04-04 13:08:28 +0100

    Started developing raytracing for the hit marker and aiming

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -4,6 +4,13 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+
+void ATankPlayerController::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+	UE_LOG(LogTemp, Warning, TEXT("Tick"));
+	AimTowardsCrosshair();
+}
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -18,9 +25,28 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+
+
 ATank * ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
 
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has side effect, is going to ray trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		
+		// TODO Tell controlled tank to aim at this point
+	}
+}
+// Get world location if linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0);
+	return true;
+}
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -18,6 +18,13 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	
 public:
 	ATank * GetControlledTank() const;
-	
+
+	//Start moving barrel so that shot would hit where crosshair intersects the world
+	void AimTowardsCrosshair();
 	virtual void BeginPlay() override;
+	virtual void Tick( float DeltaTime ) override;
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
 };
